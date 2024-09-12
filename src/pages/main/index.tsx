34819facceb301da021718d4d92311e7023c09ff
gogumastick <jarloperak@hotmail.com>
2024-09-12 20:49:23 +0900

    five

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -14,31 +14,31 @@ import React, { useEffect } from 'react';
 // }
 
 const Main = () => {
-    useEffect(() => {
-        // 우클릭 비활성화
-        const handleContextMenu = (e:any) => {
-          e.preventDefault();
-        };
+    // useEffect(() => {
+    //     // 우클릭 비활성화
+    //     const handleContextMenu = (e:any) => {
+    //       e.preventDefault();
+    //     };
 
-        // F12 및 특정 키 조합 비활성화
-        const handleKeyDown = (e:any) => {
-          if (
-            e.key === 'F12' ||
-            (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C' || e.key === 'J')) ||
-            (e.ctrlKey && e.key === 'U')
-          ) {
-            e.preventDefault();
-          }
-        };
+    //     // F12 및 특정 키 조합 비활성화
+    //     const handleKeyDown = (e:any) => {
+    //       if (
+    //         e.key === 'F12' ||
+    //         (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C' || e.key === 'J')) ||
+    //         (e.ctrlKey && e.key === 'U')
+    //       ) {
+    //         e.preventDefault();
+    //       }
+    //     };
 
-        document.addEventListener('contextmenu', handleContextMenu);
-        document.addEventListener('keydown', handleKeyDown);
+    //     document.addEventListener('contextmenu', handleContextMenu);
+    //     document.addEventListener('keydown', handleKeyDown);
 
-        return () => {
-          document.removeEventListener('contextmenu', handleContextMenu);
-          document.removeEventListener('keydown', handleKeyDown);
-        };
-      }, []);
+    //     return () => {
+    //       document.removeEventListener('contextmenu', handleContextMenu);
+    //       document.removeEventListener('keydown', handleKeyDown);
+    //     };
+    //   }, []);
 
     // 브라우저가 닫힐 때 localStorage 삭제
     // useEffect(() => {
